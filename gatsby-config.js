d163988f33f1d4d4fb8ac90737c6399ae0f3073b
miguelpduarte <miguelpduarte98@gmail.com>
2018-10-22 13:37:51 +0100

    Added typography and respective theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,11 @@
 // eslint-disable-next-line
 module.exports = {
-    plugins: ["gatsby-plugin-typography"],
+    plugins: [
+        {
+            resolve: "gatsby-plugin-typography",
+            options: {
+                pathToConfigModule: "src/utils/typography.js",
+            },
+        },
+    ],
 };
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,7 @@
 import Typography from "typography";
+import deYoungTheme from "typography-theme-de-young";
+// deYoungTheme.baseFontSize = "18px";
 
-const typography = new Typography({ baseFontSize: "18px" });
+const typography = new Typography(deYoungTheme);
 
 export default typography;
