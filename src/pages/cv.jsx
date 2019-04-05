a65e2879ca7e6e2dfeaccb8e1658e055d34d1ac1
miguelpduarte <miguelpduarte98@gmail.com>
2019-04-05 09:48:30 +0100

    Added margins to some places to improve readability

--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 import positionsStyles from "../css/positions.module.css";
+import marginStyles from "../css/margins.module.css";
 import CVExperience from "../components/cv/experience/CVExperience";
 import CVSidebar from "../components/cv/sidebar/CVSidebar";
 import CVProjects from "../components/cv/projects/CVProjects";
@@ -9,9 +10,10 @@ import CVEducation from "../components/cv/education/CVEducation";
 export default () => (
     <Layout>
         <h3>WIP - Work in Progress</h3>
+        <h3>Header here</h3>
 
         <div className={positionsStyles.unbalancedTwoColumnContainer}>
-            <div style={{marginRight: "2em"}}>
+            <div className={marginStyles.rightSmall}>
                 <CVExperience/>
                 <CVEducation/>
                 <CVProjects/>
