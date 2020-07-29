297c0f36cd880f27d0d839b07e628740cb060779
miguelpduarte <miguelpduarte98@gmail.com>
2020-07-29 12:09:24 +0100

    Add link icon to CV links

--- src/components/cv/CVSectionSubtitle.jsx
+++ src/components/cv/CVSectionSubtitle.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import marginStyles from "../../css/margins.module.css";
 import cvStyles from "./cv.module.css";
+import { FiExternalLink } from "react-icons/fi";
 
 export default ({text, link}) => (
     <h3 className={marginStyles.verticalExtraSmall}>
         <a className={cvStyles.sectionSubtitle} href={link}>
             {text}
+            <FiExternalLink style={{verticalAlign: "text-bottom"}} className={marginStyles.leftMiniscule}/>
         </a>
     </h3>
 );
--- src/components/cv/projects/CVProjectItem.jsx
+++ src/components/cv/projects/CVProjectItem.jsx
@@ -3,10 +3,16 @@ import cvStyles from "../cv.module.css";
 import marginStyles from "../../../css/margins.module.css";
 import CVSectionDate from "../CVSectionDate";
 import CVSectionText from "../CVSectionText";
+import { FiExternalLink } from "react-icons/fi";
 
 export default ({node}) => (
     <div className={marginStyles.bottomMedium}>
-        <h2><a className={cvStyles.sectionSubtitle} href={node.frontmatter.link}>{node.frontmatter.title}</a></h2>
+        <h2>
+            <a className={cvStyles.sectionSubtitle} href={node.frontmatter.link}>
+                {node.frontmatter.title}
+                <FiExternalLink style={{verticalAlign: "text-bottom"}} className={marginStyles.leftMiniscule}/>
+            </a>
+        </h2>
         <CVSectionDate start={node.frontmatter.date_start} end={node.frontmatter.date_end}/>
         <CVSectionText html={node.html}/>
     </div>
